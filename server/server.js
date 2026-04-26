c38fc5f491050f6a217e856c542178187dba6a64
noneclashofclans <rishitmohanty3@gmail.com>
2026-04-26 19:57:44 +0530

    initial ccommit

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 
-const allowedOrigins = ['http://localhost:5173', 'https://route-opimizer-rishit.netlify.app/'];
+const allowedOrigins = ['http://localhost:5173', 'https://route-opimizer-rishit.netlify.app'];
 
 app.use(cors({
     origin: (origin, cb) => {
